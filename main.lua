fbbb7f9571f14c716aacd3a82e87d2f8735891e7
lukehayes <lukepurplehayes@gmail.com>
2020-07-11 19:27:17 +0100

    Updated main.lua

--- main.lua
+++ main.lua
@@ -1,33 +1,17 @@
 Object = require "classic"
-
-local rx = 10
-
-local Point = Object:extend()
-Point.type = "Point Type"
-
-function Point:new(x,y)
-    print("new called")
-    self.x = x
-    self.y = y
-end
-
-local p = Point(100,200)
+require "setup"
 
 
 function love.load()
-    print(p.x)
-    print(p.type)
+    local objects = {}
+    recursiveEnumerate("objects", objects)
+    requireFiles(objects)
+    
 end
 
 function love.update(dt)
-    print("FPS: "..love.timer.getFPS())
-
-    for i = 1, 10000000 do
-        rx = rx * rx
-    end
-
 end
 
 
---function love.draw()
---end
+function love.draw()
+end
