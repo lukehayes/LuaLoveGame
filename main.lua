7f34a3394ad976540cf0b5016020e564ce6016fc
lukehayes <lukepurplehayes@gmail.com>
2020-07-12 14:41:49 +0100

    Cleared main

--- main.lua
+++ main.lua
@@ -1,12 +1,4 @@
-Object = require "classic"
-require "setup"
-
-
 function love.load()
-    local objects = {}
-    recursiveEnumerate("objects", objects)
-    requireFiles(objects)
-    
 end
 
 function love.update(dt)
