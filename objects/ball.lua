0b7c4cf2f8ebf1650ea03150c40f34210d569e00
lukehayes <lukepurplehayes@gmail.com>
2020-07-12 18:51:09 +0100

    Added object/ball.lua

---
+++ objects/ball.lua
@@ -0,0 +1,42 @@
+Object = require "core/classic"
+
+local Ball = Object:extend()
+
+function Ball:new(x,y)
+    self.x = x
+    self.y = y
+    self.w = 10
+    self.h = 10
+    self.speed = 200
+    self.angle = 0.01
+end
+
+function Ball:update(dt)
+    --Move the ball
+    self.x = self.x + self.speed * math.cos(self.angle) * dt
+    self.y = self.y + self.speed * math.sin(self.angle) * dt
+
+
+    if self.x < 0 or self.x > (config.window.width - self.w) or self.y < 0 or self.y > (config.window.height - self.h) then
+        self.speed = -self.speed
+    end
+
+    if love.mouse.isDown(1) then
+        self:genAngle();
+    end
+end
+
+function Ball:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+end
+
+function Ball:genAngle()
+    self.angle = love.math.random(0,359)
+    self.angle = math.rad(self.angle)
+end
+
+function Ball:__tostring()
+    return "Balls"
+end
+
+return Ball
