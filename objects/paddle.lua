5c401be28e4fe05dfa7e6d5b886bcd9267b84c1f
lukehayes <lukepurplehayes@gmail.com>
2020-07-12 19:00:18 +0100

    Added update and draw to Paddle class

--- objects/paddle.lua
+++ objects/paddle.lua
@@ -9,4 +9,16 @@ function Paddle:new(x,y)
     self.h = 10
 end
 
+function Paddle:update(dt)
+    self.x = love.mouse.getX()
+
+    if self.x < 0 or self.x > (700) then
+        self.x = self.x
+    end
+end
+
+function Paddle:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+end
+
 return Paddle
