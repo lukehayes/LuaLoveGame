5f95dc057ae684bf66604ec1f85c2ae80bac1323
lukehayes <lukepurplehayes@gmail.com>
2020-07-15 22:17:43 +0100

    Added tilemaps

--- main.lua
+++ main.lua
@@ -1,58 +1,28 @@
-local Paddle = require "objects/paddle"
-local Ball = require "objects/ball"
-local Bullet = require "objects/bullet"
+local TMX = require "tilemaps/8BitDungeon"
+local base = require "base"
+
+
+local tileQuads = {}
+local img = love.graphics.newImage("tilemaps/8BitDungeon.png")
+print(img)
+
 
 local windowWidth = config.window.width
-local p = Paddle(10, 580)
-local b = Ball(40,40)
-
-local balls = {}
-local bullets = {}
 
 function love.load(args)
-
-    if not args[1] then args[1] = 0 end
-
-    for i=1,args[1] do
-        local rx = love.math.random(10,770)
-        local ry = love.math.random(10,580)
-        local rr = love.math.random(0,1)
-        local rg = love.math.random(0,1)
-        local rb = love.math.random(0,1)
-        local ball = Ball(rx,ry)
-        ball.color.r = rr
-        ball.color.g = rg
-        ball.color.b = rb
-
-        table.insert(balls, ball)
-        local bullet = Bullet(p.x + (p.w / 2),p.y)
-        table.insert(bullets, bullet)
+    for i = 0,40,8 do
+        local quad = love.graphics.newQuad(i,0,8,8, img:getDimensions())
+        table.insert(tileQuads, quad);
     end
-
-    love.mouse.setVisible(false)
 end
 
 function love.update(dt)
-    p:update(dt)
-    b:update(dt)
-
-    if love.mouse.isDown(1) then
-        local bullet = Bullet(p.x,p.y)
-        table.insert(bullets, bullet)
-    end
-
-
-    for _,bullet in pairs(bullets) do
-        bullet:update(dt)
-    end
 end
 
 
 function love.draw()
     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
-    p:draw()
-    b:draw()
-    for _,bullet in pairs(bullets) do
-        bullet:draw()
+    love.graphics.draw(img)
+    for k,v in pairs(tileQuads) do
     end
 end
---
+++ tilemaps/8BitDungeon.lua
@@ -0,0 +1,121 @@
+return {
+  version = "1.4",
+  luaversion = "5.1",
+  tiledversion = "1.4.1",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 8,
+  tileheight = 8,
+  nextlayerid = 3,
+  nextobjectid = 7,
+  properties = {},
+  tilesets = {
+    {
+      name = "8BitDungeon",
+      firstgid = 1,
+      filename = "8BitDungeon.tsx",
+      tilewidth = 8,
+      tileheight = 8,
+      spacing = 0,
+      margin = 0,
+      columns = 8,
+      image = "8BitDungeon.png",
+      imagewidth = 64,
+      imageheight = 8,
+      objectalignment = "unspecified",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 8,
+        height = 8
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 8,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 20,
+      height = 20,
+      id = 1,
+      name = "Tiles",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "Objects",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      objects = {
+        {
+          id = 4,
+          name = "Door",
+          type = "",
+          shape = "rectangle",
+          x = 103.81,
+          y = 79.94,
+          width = 8,
+          height = 8,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 5,
+          name = "Spawn",
+          type = "",
+          shape = "rectangle",
+          x = 40,
+          y = 79.5,
+          width = 8,
+          height = 8,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
